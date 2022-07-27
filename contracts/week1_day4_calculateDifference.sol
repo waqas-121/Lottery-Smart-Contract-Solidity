8ed84109ee7474d9742a2f3aa4ae5e4428389eff
"waqas" <"w.akber801@gmail.com">
2022-07-28 03:10:34 +0500

    day_4

---
+++ contracts/week1_day4_calculateDifference.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day4 {
+
+function evaluate(int a,int b) public pure returns(int difference){
+    difference =(a+b)-(a-b);
+    return difference;
+}
+    
+}
